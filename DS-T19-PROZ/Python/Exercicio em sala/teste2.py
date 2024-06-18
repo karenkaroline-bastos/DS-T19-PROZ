28fdb3b9dd47610b5ca455f6421e5c379c7bb115
Karen Bastos <137943069+karenkaroline-bastos@users.noreply.github.com>
2024-06-18 18:57:38 -0300

    Add files via upload

---
+++ DS-T19-PROZ/Python/Exercicio em sala/teste2.py
@@ -0,0 +1,13 @@
+valFahrenheit = float
+
+valCelsius = float(input('Digite o valor em Celsius:'))
+
+valFahrenheit = (((valCelsius*9)/5)+32)
+
+print('O valor em Fahrenheit é:',valFahrenheit)
+
+
+#Caracter= str
+#real=float
+#inteiro=int
+#logico=bool
