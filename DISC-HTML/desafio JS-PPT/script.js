7e4fdbe62618d71cd169070791bd440961af68a2
karen <karenkaroline.bastos@gmail.com>
2024-09-15 17:00:32 -0300

    feat: Desafio PPT

---
+++ DISC-HTML/desafio JS-PPT/script.js
@@ -0,0 +1,41 @@
+function jogar(escolhaUsuario) {
+    const opcoes = ['pedra', 'papel', 'tesoura'];
+    const escolhaComputador = opcoes[Math.floor(Math.random() * 3)];
+
+    let resultado = '';
+
+        if (escolhaUsuario === escolhaComputador){
+            resultado = 'Empate!';  
+        }else if (escolhaUsuario === 'pedra' && escolhaComputador === 'tesoura'){
+            resultado = 'Você ganhou!';
+        }else if (escolhaUsuario === 'papel' && escolhaComputador === 'pedra'){
+            resultado = 'Você ganhou!';
+        }else if (escolhaUsuario === 'tesoura' && escolhaComputador === 'papel'){
+            resultado = 'Você ganhou!';
+        }else if (escolhaUsuario === 'pedra' && escolhaComputador === 'papel'){
+            resultado = 'Você Perdeu!';  
+        }else if (escolhaUsuario === 'papel' && escolhaComputador === 'tesoura'){
+            resultado = 'Você Perdeu!';
+        }   else if (escolhaUsuario === 'tesoura' && escolhaComputador === 'pedra'){
+            resultado = 'Você Perdeu!';
+        };
+
+        let resultadoElement = document.getElementById('resultado');
+        resultadoElement.innerHTML = resultado;
+
+
+    document.getElementById('resultado').innerHTML = `
+        Você escolheu <strong>${escolhaUsuario}</strong>.<br>
+        O computador escolheu <strong>${escolhaComputador}</strong><br>
+        <strong>${resultado}</strong>`;
+
+    function exibirImagem(elementId, escolha) {
+        const divImagem = document.getElementById(elementId);
+        divImagem.innerHTML = `<img width="200px" src="${escolha}.jpg" alt="${escolha}">`;
+
+
+    }
+
+    exibirImagem('imagem1', escolhaUsuario);
+    exibirImagem('imagem2', escolhaComputador);
+}
